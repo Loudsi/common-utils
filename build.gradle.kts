d50b2490fd1cbcb7f5f451a619a39c027b8cc410
Loudsi <2044858-Loudsi@users.noreply.gitlab.com>
2020-06-21 17:40:12 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ publishing {
     repositories {
         maven {
             //url = uri("https://gitlab.com/api/v4/projects/19495568/packages/maven")
-            url = uri("https://gitlab.com/api/v4/groups/org.loudsi/-/packages/maven")
+            url = uri("https://gitlab.com/api/v4/groups/loudsi-group/-/packages/maven")
             credentials(HttpHeaderCredentials::class) {
                 name = "Job-Token"
                 value = CI_TOKEN
