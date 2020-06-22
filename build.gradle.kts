aebaa841ed812cd40266283fa1f3e764350191e7
Loudsi <louismaistre@gmail.com>
2020-06-22 02:48:28 +0200

    publishing packages to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/Loudsi/common-utils")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
